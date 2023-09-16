dd2014a5b5d8584c6d144745d00bd3c1790fa801
JGamaa <jeffersongama.eng@gmail.com>
2023-09-16 00:31:35 -0300

    Consegui

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log ("amigo")
+
+
+let Nome = "JGamaa"
+let Xp = 10001
+let Niveis = ["Ferro", "Bronze", "Prata", "Ouro", "Platina", "Ascendente", "Imortal", "Radiante"]
+
+if (Xp < 1000){
+console.log ("O Herói de nome " + Nome + " está no nível de " + Niveis[0])
+}
+    else if ( 1000 > Xp || Xp <=2000){
+    console.log ("O Herói de nome " + Nome + " está no nível de " + Niveis[1])
+}
+        else if ( 2000 > Xp || Xp <=5000){
+
+            console.log ("O Herói de nome " + Nome + " está no nível de " + Niveis[2])
+}
+            else if ( 6000 > Xp || Xp<=7000){
+
+                console.log ("O Herói de nome " + Nome + " está no nível de " + Niveis[3])
+}
+                else if ( 7000 > Xp || Xp<=8000){
+
+                    console.log ("O Herói de nome " + Nome + " está no nível de " + Niveis[4])
+}
+            else if ( 8000 > Xp || Xp <=9000){
+
+                console.log ("O Herói de nome " + Nome + " está no nível de " + Niveis[5])
+}
+        else if ( 9000 > Xp || Xp <=10000){
+
+            console.log ("O Herói de nome " + Nome + " está no nível de " + Niveis[6])
+}
+else if (Xp > 10000){
+
+    console.log ("O Herói de nome " + Nome + " está no nível de " + Niveis[7])
+}
